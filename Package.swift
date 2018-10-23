8f8a0b0d37b7abafe09d987066f65aa2201ff7a2
Ryuichi Sai <ryuichi@ryuichisai.com>
2018-10-23 16:32:32 -0500

    Catch up swift 4.2 changes

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 /*
-   Copyright 2017 Ryuichi Laboratories and the Yanagiba project contributors
+   Copyright 2017-2018 Ryuichi Laboratories and the Yanagiba project contributors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -54,5 +54,5 @@ let package = Package(
       ]
     ),
   ],
-  swiftLanguageVersions: [4]
+  swiftLanguageVersions: [.v4_2]
 )
