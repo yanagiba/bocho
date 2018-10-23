e8cb6fdfcb1cb1819a0b7027edf4ffc2965075e5
Ryuichi Sai <ryuichi@ryuichisai.com>
2018-10-23 18:52:34 -0500

    Use osx 10.14

--- runXcodeTest.sh
+++ runXcodeTest.sh
@@ -2,4 +2,4 @@
 
 make xcodegen
 WORKING_DIRECTORY=$(PWD) xcodebuild -project bocho.xcodeproj -scheme bocho-Package clean
-WORKING_DIRECTORY=$(PWD) xcodebuild -project bocho.xcodeproj -scheme bocho-Package -sdk macosx10.13 -destination arch=x86_64 -configuration Debug -enableCodeCoverage YES test
+WORKING_DIRECTORY=$(PWD) xcodebuild -project bocho.xcodeproj -scheme bocho-Package -sdk macosx10.14 -destination arch=x86_64 -configuration Debug -enableCodeCoverage YES test
